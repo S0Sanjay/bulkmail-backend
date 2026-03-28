70cccf3a126d9a37f0482abf516521ef4e538183
S0Sanjay <sanjaysarvanan9095364074@gmail.com>
2026-03-28 21:20:22 +0530

    fix CORS origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "*",
+  origin: "https://bulkmail-frontend-eosin.vercel.app",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
